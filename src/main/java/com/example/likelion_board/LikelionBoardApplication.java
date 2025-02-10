f1dbdfc3106c60530faa26fef3ccd1bc185a6599
jihyun <jihyun3066@naver.com>
2025-02-10 10:58:24 +0900

    first commit

---
+++ src/main/java/com/example/likelion_board/LikelionBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelion_board/LikelionBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
